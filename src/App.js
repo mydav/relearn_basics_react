5b7e00ec9223cf219aa1d19317df83b860b58d3d
vadym bardukov <19vad91@gmail.com>
2023-08-01 23:24:35 -0400

    implemented simple counter functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React from 'react';
 import './index.scss';
 
 function App() {
+  const [count, setCount] = React.useState(0)
+
+  const onClickPlus = () => {
+    setCount(count + 1)
+  }
+
+  const onClickMinus = () => {
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Counter:</h2>
-        <h1>0</h1>
-        <button className="minus">- Minus</button>
-        <button className="plus">Plus +</button>
+        <h1>{count}</h1>
+        <button onClick = {onClickMinus} className="minus">- Minus</button>
+        <button onClick = {onClickPlus} className="plus">Plus +</button>
       </div>
     </div>
   );
