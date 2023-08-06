0f5531997fcfe86406143d46482842000bf5eb7e
vadym bardukov <19vad91@gmail.com>
2023-08-06 16:35:16 -0400

    search, addition, send and other functional implementation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
+// Users list: https://reqres.in/api/users
 
 function App() {
   const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
   const [isLoading, setLoading] = React.useState(true);
+  const [success, setSuccess] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
 
 
   React.useEffect (() => {
@@ -23,10 +26,38 @@ function App() {
     .finally( () => setLoading(false));
   }, []);
 
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value)
+  };
+
+  const onClickSend = () => {
+    setSuccess(true)
+  };
+
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvites((prev) => [...prev, id]);
+    }
+  };
+
   return (
     <div className="App">
-      <Users items={users} isLoading={isLoading} />
-      {/* <Success /> */}
+      
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users 
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue} 
+          items={users} 
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSend={onClickSend} />
+      )}
     </div>
   );
 }
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export const Success = ({ count }) => {
   return (
-    <div class="success-block">
+    <div className="success-block">
       <img src="/assets/success.svg" alt="Success" />
       <h3>Success!</h3>
-      <p>All {count} invites has been sent.</p>
-      <button className="send-invite-btn">Back</button>
+      <p>{count} invites has been sent.</p>
+      <button onClick={() => window.location.reload()} className="send-invite-btn">Back</button>
     </div>
   );
 };
